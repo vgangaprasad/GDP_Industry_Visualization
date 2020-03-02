4b6f3ad80c239124059c0e79b5dca885bdd01c53
Vijaybabu Gangaprasad <vijay.gangaprasad@outlook.com>
2020-03-02 11:54:38 -0800

    Removed few folders which are not required

--- static/app.js
+++ static/app.js
@@ -289,13 +289,8 @@ function lineChart(state, industryId) {
           {
             data: numbersline,
             label: descripsline,
-            backgroundColor: ['rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'],
-            borderColor: "red",
+            backgroundColor: "#ff80a6",
+            borderColor: "yellow",
               }
         ]
       },
